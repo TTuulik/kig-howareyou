e153014e81c172b3434746f9db9ddf0dd4f0cbf6
irisjk <94617951+irisjk@users.noreply.github.com>
2022-10-05 19:46:02 +0300

    Down2 icon size fix

--- pages/tuju/index.vue
+++ pages/tuju/index.vue
@@ -122,22 +122,22 @@ const down2 = [
   {
     title: "",
     fill: "#DFF996",
-    icon: "/assets/leaf-outline.svg", size 40px,
+    icon: "/assets/leaf-outline.svg", size 40 px,
   },
   {
     title: "",
     fill: "#FEF08A",
-    icon: "/assets/artist-palette.svg", size 40px,
+    icon: "/assets/artist-palette.svg", size 40 px,
   },
   {
     title: "",
     fill: "#BBF7D0",
-    icon: "/assets/brain.svg", size 40px,
+    icon: "/assets/brain.svg", size 40 px,
   },
   {
     title: "",
     fill: "#F5E3A0",
-    icon: "/assets/body-outline.svg", size 40px,
+    icon: "/assets/body-outline.svg", size 40 px,
   },
 ];
 </script>
