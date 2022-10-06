248a354a72e0d673206a3bcfe014e69166c15eb6
karmelp <101389533+karmelp@users.noreply.github.com>
2022-10-06 13:38:17 +0300

    responsive

--- pages/emotionally/index.vue
+++ pages/emotionally/index.vue
@@ -4,17 +4,8 @@ definePageMeta({ layout: "emotionally" });
 </script>
 <template>
   <div>
-    <img src="/assets/css/Background.png" class="h-screen object-cover fixed -z-10"/>
-    <div
-      style="
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 812px;
-        max-width: 375px;
-        height: 100%;
-      "
-    >
+    <img src="/assets/css/Background.png" class="h-screen object-cover fixed -z-10" style="width:100%;"/>
+    <div style="display: flex; justify-content: center; align-items: center; min-height: 812px; width: 100%; height: 100%;">
       <nuxt-link to="/emotionally/feelings" style="height: 185px; width: 100%; background: #424f3db2; display: flex; justify-content: center; align-items: center; flex-direction: column; box-shadow: inset 0px -4px 4px rgba(0, 0, 0, 0.25), inset 0px 4px 4px rgba(0, 0, 0, 0.25);">
         <img src="/assets/css/Logo.png" alt="Emotionally logo" class="w-64" />
         <p style="color: #fff; text-align: center; font-size: 16px">
