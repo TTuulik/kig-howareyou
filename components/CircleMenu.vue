9ea296c6a6db973abc56a85bc96742ba72d823cd
irisjk <94617951+irisjk@users.noreply.github.com>
2022-10-06 15:15:47 +0300

    Text-box fix

--- components/CircleMenu.vue
+++ components/CircleMenu.vue
@@ -8,13 +8,13 @@ defineEmits(["select"]);
   <div class="grid grid-cols-2 gap-4 p-8">
     <div
       v-for="(option, i) in options"
-      class="aspect-square bg-red-500 grid place-items-center text-center text-8xl p-6"
+      class="aspect-square bg-red-500 grid place-items-center text-center text-4xl p-6"
       :class="[
         [
-          'rounded-tl-[550px]',
-          'rounded-tr-[550px]',
-          'rounded-bl-[550px]',
-          'rounded-br-[550px]',
+          'rounded-tl-[150px]',
+          'rounded-tr-[150px]',
+          'rounded-bl-[150px]',
+          'rounded-br-[150px]',
         ][i % 4],
       ]"
       :style="{ background: option.fill }"
