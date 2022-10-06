c5de88ef749ee4b3f60b0beff22cb5532ffc403a
irisjk <94617951+irisjk@users.noreply.github.com>
2022-10-06 15:50:38 +0300

    Corners-fixes

--- components/CircleMenu.vue
+++ components/CircleMenu.vue
@@ -4,16 +4,16 @@ defineEmits(["select"]);
 </script>
 
 <template>
-  <div class="grid grid-cols-2 gap-4 p-8">
+  <div class="grid grid-cols-2 gap-1 p-8">
     <div
       v-for="(option, i) in options"
       class="aspect-square bg-red-500 grid place-items-center text-center text-lg p-6"
       :class="[
         [
-          'rounded-tl-[50px]',
-          'rounded-tr-[50px]',
-          'rounded-bl-[50px]',
-          'rounded-br-[50px]',
+          'rounded-tl-[100px]',
+          'rounded-tr-[100px]',
+          'rounded-bl-[100px]',
+          'rounded-br-[100px]',
         ][i % 4],
       ]"
       :style="{ background: option.fill }"
