dacb9eb84409205c2fa8ed7d7787a16f11a40826
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-10-06 14:46:17 +0300

    fix

---
+++ components/CircleMenu.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { interpolateSinebow } from "d3";
+const { options } = defineProps(["options"]);
+defineEmits(["select"]);
+</script>
+
+<template>
+  <div class="grid grid-cols-2 gap-1 p-4">
+    <div
+      v-for="(option, i) in options"
+      class="aspect-square bg-red-500 grid place-items-center text-center p-6"
+      :class="[
+        [
+          'rounded-tl-[50px]',
+          'rounded-tr-[50px]',
+          'rounded-bl-[50px]',
+          'rounded-br-[50px]',
+        ][i % 4],
+      ]"
+      :style="{ background: option.fill }"
+      @click="$emit('select', option)"
+    >
+      <div class="opacity-80">{{ option.title }}</div>
+    </div>
+  </div>
+</template>
