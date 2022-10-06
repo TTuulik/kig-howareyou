8090e439796dfc41e873ed4068e894c5b9320117
Kristjan Jansen <kristjan.jansen@gmail.com>
2022-10-06 15:48:28 +0300

    fix

--- pages/ketas/index.vue
+++ pages/ketas/index.vue
@@ -130,62 +130,64 @@ const showMessage = $ref(true);
 </script>
 
 <template>
-  <div class="grid grid-rows-[1fr_auto] h-screen h-screen-ios">
-    <div class="flex items-center justify-center overflow-hidden">
-      <TestWheel
-        class="w-[180vw] md:w-[60vw] -ml-[90vw] md:ml-0"
-        :size="500"
-        :style="{
-          transform: 'rotate(' + rotation + 'deg)',
-        }"
-      >
-        <TestSlices
-          @select="onSelect1"
-          :selection="selection"
-          :options="options"
-          :inner="0"
-          :outer="80"
-        />
-        <TestSlices
-          @select="onSelect1"
-          :selection="selection"
-          :options="options2"
-          :inner="80"
-          :outer="160"
-        />
-        <TestSlices
-          @select="onSelect1"
-          :selection="selection"
-          :options="options3"
-          :inner="160"
-          :outer="240"
-        />
-        <!-- <circle r="250" />
+  <div>
+    <div class="grid grid-rows-[1fr_auto] h-screen h-screen-ios">
+      <div class="flex items-center justify-center overflow-hidden">
+        <TestWheel
+          class="w-[180vw] md:w-[60vw] -ml-[90vw] md:ml-0"
+          :size="500"
+          :style="{
+            transform: 'rotate(' + rotation + 'deg)',
+          }"
+        >
+          <TestSlices
+            @select="onSelect1"
+            :selection="selection"
+            :options="options"
+            :inner="0"
+            :outer="80"
+          />
+          <TestSlices
+            @select="onSelect1"
+            :selection="selection"
+            :options="options2"
+            :inner="80"
+            :outer="160"
+          />
+          <TestSlices
+            @select="onSelect1"
+            :selection="selection"
+            :options="options3"
+            :inner="160"
+            :outer="240"
+          />
+          <!-- <circle r="250" />
         <rect x="-100" y="-100" width="200" height="200" fill="white" />
         <rect x="-90" y="-90" width="20" height="20" /> -->
-      </TestWheel>
+        </TestWheel>
+      </div>
+      <button
+        class="fixed top-8 right-8 text-4xl font-bold duration-1000 transition"
+        :class="[showMessage ? 'opacity-[0.01] -translate-y-4' : 'opacity-30']"
+        @click="showMessage = !showMessage"
+      >
+        ◯
+      </button>
+      <div
+        @click="showMessage = false"
+        class="duration-1000 transition fixed top-8 left-8 right-8 md:w-1/3 text-5xl font-bold opacity-70"
+        :class="[
+          showMessage
+            ? 'opacity-80'
+            : 'pointer-events-none opacity-0 -translate-y-8',
+        ]"
+      >
+        Su tunded on sinu omad ja kõik on väärt mäletamist. Märka ja märgi
+        kuidas sa ennast tunned.
+      </div>
     </div>
-    <div class="flex justify-center pb-8">
+    <div class="fixed right-4 bottom-12 left-4 flex justify-center">
       <Slider type="range" v-model="rotation" max="360" />
-    </div>
-    <button
-      class="fixed top-8 right-8 text-4xl font-bold duration-1000 transition"
-      :class="[showMessage ? 'opacity-[0.01] -translate-y-4' : 'opacity-30']"
-      @click="showMessage = !showMessage"
-    >
-      ◯
-    </button>
-    <div
-      @click="showMessage = false"
-      class="duration-1000 transition fixed top-8 left-8 right-8 md:w-1/3 text-5xl font-bold opacity-70"
-      :class="[
-        showMessage
-          ? 'opacity-80'
-          : 'pointer-events-none opacity-0 -translate-y-8',
-      ]"
-    >
-      Su tunded on sinu omad ja kõik on väärt mäletamist. Märka ja märgi kuidas
-      sa ennast tunned.
     </div>
   </div>
 </template>
